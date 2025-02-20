660421bd24bc96d795a628625e3378667cd44bc1
Dhruv <dhruvpadhiar521@gmail.com>
2025-02-20 20:33:13 +0530

    second commit

--- Context/src/App.jsx
+++ Context/src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import UserContextProvider from './context/UserContextProvider'
 import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Context</h1>
-    </>
+    <UserContextProvider>
+      <h1>Hello</h1>
+      <Login />
+      <Profile/>
+    </UserContextProvider>
+    
   )
 }
 
