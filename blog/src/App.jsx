9c8ae7971b4269b9e9f15fc9d8fa9c21a4ca1697
Dhruv <dhruvpadhiar521@gmail.com>
2025-02-23 21:01:26 +0530

    fourth commit

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,12 +1,41 @@
+import { useState, useEffect } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSilce'
 import './App.css'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import {Outlet} from 'react-router-dom'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>hello world</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-white'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          TODO: {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
+
+
 }
 
 export default App
